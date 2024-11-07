1b735f1fd867ff2980251c26cd7c0ecad3ac5747
NickooMar <nicoo.marsili@gmail.com>
2024-11-06 22:27:25 -0300

    feat: create startup - useActionState

---
+++ components/StartupForm.tsx
@@ -0,0 +1,144 @@
+"use client";
+
+import React, { useActionState, useState } from "react";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { Send } from "lucide-react";
+import { formSchema } from "@/lib/validation";
+import { z } from "zod";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import { createIdea } from "@/lib/actions";
+
+const StartupForm = () => {
+  const [errors, setErrors] = useState<Record<string, string>>({});
+  const { toast } = useToast();
+  const router = useRouter();
+
+  const handleFormSubmit = async (prevState: any, formData: FormData) => {
+    try {
+      const formValues = {
+        title: formData.get("title") as string,
+        description: formData.get("description") as string,
+        category: formData.get("category") as string,
+        link: formData.get("link") as string,
+      };
+
+      await formSchema.parseAsync(formValues);
+
+      toast({
+        title: "Success",
+        description: "Your startup has been submitted",
+      });
+
+      const result = await createIdea(prevState, formData);
+
+      if (result.status === "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup has been submitted",
+        });
+        router.push(`/startup/${result._id}`);
+      }
+
+      return result;
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        const fieldErrors = error.flatten().fieldErrors;
+        setErrors(fieldErrors as unknown as Record<string, string>);
+        toast({
+          title: "Error",
+          description: "Invalid form data",
+          variant: "destructive",
+        });
+
+        return { ...prevState, error: "Invalid form data", status: "ERROR" };
+      }
+
+      toast({
+        title: "Error",
+        description: "An error occurred",
+        variant: "destructive",
+      });
+
+      return { ...prevState, error: "An error occurred", status: "ERROR" };
+    }
+  };
+
+  const [state, formAction, isPending] = useActionState(handleFormSubmit, {
+    error: "",
+    status: "INITIAL",
+  });
+
+  return (
+    <form action={formAction} className="startup-form">
+      <div>
+        <label htmlFor="title" className="startup-form_label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="startup-form_input"
+          required
+          placeholder="Startup Title"
+        />
+        {errors.title && <p className="startup-form_error">{errors.title}</p>}
+      </div>
+
+      <div>
+        <label htmlFor="description" className="startup-form_label">
+          Description
+        </label>
+        <Textarea
+          id="description"
+          name="description"
+          className="startup-form_textarea"
+          required
+          placeholder="Startup Description"
+        />
+        {errors.description && (
+          <p className="startup-form_error">{errors.description}</p>
+        )}
+      </div>
+
+      <div>
+        <label htmlFor="category" className="startup-form_label">
+          Category
+        </label>
+        <Input
+          id="category"
+          name="category"
+          className="startup-form_input"
+          required
+          placeholder="Startup Category (Tech, Health, etc)"
+        />
+        {errors.category && (
+          <p className="startup-form_error">{errors.category}</p>
+        )}
+      </div>
+
+      <div>
+        <label htmlFor="link" className="startup-form_label">
+          Image URL
+        </label>
+        <Input
+          id="link"
+          name="link"
+          className="startup-form_input"
+          required
+          placeholder="Startup Image URL"
+        />
+        {errors.link && <p className="startup-form_error">{errors.link}</p>}
+      </div>
+
+      <Button type="submit" className="startup-form_btn" disabled={isPending}>
+        {isPending ? "Submitting..." : "Submit"}
+        <Send className="size-6 ml-2" />
+      </Button>
+    </form>
+  );
+};
+
+export default StartupForm;
